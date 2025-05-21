9690057a01e721e998ce54f9c57f9c62dac42c5f
Lucas <lucas.iferreir@gmail.com>
2025-05-21 10:22:02 -0300

    Domain Model

--- src/main/java/io/github/lucasferreira/dslist/controllers/GameList.java
+++ src/main/java/io/github/lucasferreira/dslist/controllers/GameList.java
@@ -1,13 +1,11 @@
 package io.github.lucasferreira.dslist.controllers;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.util.Objects;
 
 @Entity
+@Table(name = "tb_game_list")
 public class GameList {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
