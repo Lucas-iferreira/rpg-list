e08abb7fae7ca9daf8694ab5f8e19434c638d7be
Lucas <lucas.iferreir@gmail.com>
2025-05-21 10:59:28 -0300

    Created GameListDTO

---
+++ src/main/java/io/github/lucasferreira/dslist/dto/GameListDTO.java
@@ -0,0 +1,37 @@
+package io.github.lucasferreira.dslist.dto;
+
+import io.github.lucasferreira.dslist.entities.GameList;
+import org.springframework.beans.BeanUtils;
+
+import java.util.Objects;
+
+public class GameListDTO {
+
+    private Long id;
+    private String name;
+
+    public GameListDTO() {
+    }
+
+    public GameListDTO(GameList entity) {
+        id = entity.getId();
+        name = entity.getName();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
