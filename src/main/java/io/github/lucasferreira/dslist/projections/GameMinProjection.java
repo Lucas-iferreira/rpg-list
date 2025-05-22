d121fa479f18772e406ab069be0a25903e3cd288
Lucas <lucas.iferreir@gmail.com>
2025-05-21 23:25:52 -0300

    Homolog ok

--- src/main/java/io/github/lucasferreira/dslist/projections/GameMinProjection.java
+++ src/main/java/io/github/lucasferreira/dslist/projections/GameMinProjection.java
@@ -3,7 +3,7 @@ package io.github.lucasferreira.dslist.projections;
 public interface GameMinProjection {
     Long getId();
     String getTitle();
-    Integer getYear();
+    Integer getGameYear();
     String getImgUrl();
     String getShortDescription();
     Integer getPosition();
